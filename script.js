a4a45d47925c122f7e15f3bf69fa8b1684811ed7
Rikki Crayton <rikkicrayton@Rikkis-MacBook-Air.local>
2022-04-01 12:17:52 -0400

    Commented out lines 23-83 and added api & line to print to the console for testing

--- script.js
+++ script.js
@@ -5,76 +5,78 @@ const drDetailsContent = document.querySelector(".doctor-details-content");
 const drInfoCloseBtn = document.getElementById("drInfo-close-btn");
 
 //Event Listner -- here to do SOMETHING upon user//
-searchBtn.addEventListener("click", getDrList);
-drList.addEventListener("click",getDrInfo);
-drInfoCloseBtn.addEventListener("click", function() {
-  drDetailsContent.parentElement.classList.remove("showDrInfo");  
-});
+searchBtn.addEventListener('click', getDrList);
+// drList.addEventListener("click",getDrInfo);
+// drInfoCloseBtn.addEventListener("click", function() {
+//   drDetailsContent.parentElement.classList.remove("showDrInfo");  
+// });
 
 
 // get doctor list that matches with the dx or condition
 function getDrList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(//`https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchInputTxt}`)
+    fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.1&taxonomy_description=${searchInputTxt}`)
     .then(response => response.json())
-    .then(data => {
-        let html = "";
-        //name of the object data.[here]
-        if(data.doctors){
-            //will need to change line 27, 29 and 32 with object name from API
-            data.doctors.forEach(meal => {
-                html += `
-                    <div class = "doctor-item" data-id = "${meal.idMeal}">
-                        <div class = "dr-img">
-                            <img src = "${meal.strMealThumb}" alt = "Doctor">
-                        </div>
-                        <div class = "doctor-name">
-                            <h3>${meal.strMeal}</h3>
-                            <a href = "#" class = "dr-btn">Get Recipe</a>
-                        </div>
-                    </div>
-                `;
-            });
-            drList.classList.remove('notFound');
-        } else{
-            html = "Sorry, we didn't find any physicians with your requested needs!";
-            drList.classList.add('notFound');
-        }
-
-        drList.innerHTML = html;
-    });
-}
-
-
-// get information of the doctor -- line 56 needs to have the correct object name, line 54 will need to have the correct API directory
-function getDrInfo(e){
-    e.preventDefault();
-    if(e.target.classList.contains('dr-btn')){
-        let doctorItem = e.target.parentElement.parentElement;
-        fetch(//`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
-        .then(response => response.json())
-        .then(data => drListModal(data.meals));
+    .then(function (data) {
+        console.log(data);})
     }
-}
+//        let html = "";
+//         //name of the object data.[here]
+//         if(data.doctors){
+//             //will need to change line 27, 29 and 32 with object name from API
+//             data.doctors.forEach(meal => {
+//                 html += `
+//                     <div class = "doctor-item" data-id = "${results.first_name}">
+//                         <div class = "dr-img">
+//                             <img src = "${meal.strMealThumb}" alt = "Doctor">
+//                         </div>
+//                         <div class = "doctor-name">
+//                             <h3>${meal.strMeal}</h3>
+//                             <a href = "#" class = "dr-btn">Get Recipe</a>
+//                         </div>
+//                     </div>
+//                 `;
+//             });
+//             drList.classList.remove('notFound');
+//         } else{
+//             html = "Sorry, we didn't find any physicians with your requested needs!";
+//             drList.classList.add('notFound');
+//         }
 
-// create a modal -- meal parameter will need to be changed to object name
-function drListModal(meal){
-    console.log(meal);
-    meal = meal[0];
-    let html = `
-        <h2 class = "doctor-name">${meal.strMeal}</h2>
-        <p class = "doctor-specialty">${meal.strCategory}</p>
-        <div class = "recipe-instruct">
-            <h3>Instructions:</h3>
-            <p>${meal.strInstructions}</p>
-        </div>
-        <div class = "doctor-profile-img">
-            <img src = "${meal.strMealThumb}" alt = "">
-        </div>
-        <div class = "doctor-webpage-link">
-            <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
-        </div>
-    `;
-    drDetailsContent.innerHTML = html;
-    drDetailsContent.parentElement.classList.add('showDrInfo');
-}
+//         drList.innerHTML = html;
+//     });
+// }
+
+
+// // get information of the doctor -- line 56 needs to have the correct object name, line 54 will need to have the correct API directory
+// function getDrInfo(e){
+//     e.preventDefault();
+//     if(e.target.classList.contains('dr-btn')){
+//         let doctorItem = e.target.parentElement.parentElement;
+//         fetch(//`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealItem.dataset.id}`)
+//         .then(response => response.json())
+//         .then(data => drListModal(data.meals))
+//     }
+// }
+
+// // create a modal -- meal parameter will need to be changed to object name
+// function drListModal(meal){
+//     console.log(meal);
+//     meal = meal[0];
+//     let html = `
+//         <h2 class = "doctor-name">${meal.strMeal}</h2>
+//         <p class = "doctor-specialty">${meal.strCategory}</p>
+//         <div class = "recipe-instruct">
+//             <h3>Instructions:</h3>
+//             <p>${meal.strInstructions}</p>
+//         </div>
+//         <div class = "doctor-profile-img">
+//             <img src = "${meal.strMealThumb}" alt = "">
+//         </div>
+//         <div class = "doctor-webpage-link">
+//             <a href = "${meal.strYoutube}" target = "_blank">Watch Video</a>
+//         </div>
+//     `;
+//     drDetailsContent.innerHTML = html;
+//     drDetailsContent.parentElement.classList.add('showDrInfo');
+// }
