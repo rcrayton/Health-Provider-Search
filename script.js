1e26daa5c2a423d0e006dd92a9ab8e92bf65dfc0
Rikki Crayton <rikkicrayton@Rikkis-Air.attlocal.net>
2022-04-06 18:52:27 -0400

    Added and removed filter button

--- script.js
+++ script.js
@@ -6,46 +6,45 @@ const drInfoCloseBtn = document.getElementById("drInfo-close-btn");
 
 //Event Listner -- here to do SOMETHING upon user//
 searchBtn.addEventListener('click', getDrList);
-// drList.addEventListener("click",getDrInfo);
+//drList.addEventListener("click",getDrInfo);
 // drInfoCloseBtn.addEventListener("click", function() {
 //   drDetailsContent.parentElement.classList.remove("showDrInfo");  
 // });
 
 
 // get doctor list that matches with the dx or condition
+
 function getDrList(){
     let searchInputTxt = document.getElementById('search-input').value.trim();
-    fetch(`https://npiregistry.cms.hhs.gov/api/?version=2.1&taxonomy_description=${searchInputTxt}`)
-    .then(response => response.json())
-    .then(function (data) {
-        console.log(data);})
-    }
-//        let html = "";
-//         //name of the object data.[here]
-//         if(data.doctors){
-//             //will need to change line 27, 29 and 32 with object name from API
-//             data.doctors.forEach(meal => {
-//                 html += `
-//                     <div class = "doctor-item" data-id = "${results.first_name}">
-//                         <div class = "dr-img">
-//                             <img src = "${meal.strMealThumb}" alt = "Doctor">
-//                         </div>
-//                         <div class = "doctor-name">
-//                             <h3>${meal.strMeal}</h3>
-//                             <a href = "#" class = "dr-btn">Get Recipe</a>
-//                         </div>
-//                     </div>
-//                 `;
-//             });
-//             drList.classList.remove('notFound');
-//         } else{
-//             html = "Sorry, we didn't find any physicians with your requested needs!";
-//             drList.classList.add('notFound');
-//         }
+    fetch(`https://randomuser.me/api/?inc=name,location,picture,cell&results=20${searchInputTxt}`)
+        .then(response => response.json())
+        .then((data) => {
+        let html = "";
+        //name of the object data.[here]
+        if(data.results){
+            //will need to change line 27, 29 and 32 with object name from API
+            data.results.forEach(doctor => {
+                html += `
+                    <div class = "doctor-item" data-id = "${doctor.name.first}">
+                        <div class = "dr-img">
+                            <img src = "${doctor.picture.large}" alt = "Doctor">
+                        </div>
+                        <div class = "doctor-name">
+                            <h3>Dr. ${doctor.name.first} ${doctor.name.last}</h3>
+                            <a href = "#" class = "dr-btn">Get Additional Info</a>
+                        </div>
+                    </div>
+                `;
+            });
+            drList.classList.remove('notFound');
+        } else{
+            html = "Sorry, we didn't find any physicians with your requested needs!";
+            drList.classList.add('notFound');
+        }
 
-//         drList.innerHTML = html;
-//     });
-// }
+        drList.innerHTML = html;
+    });
+}
 
 
 // // get information of the doctor -- line 56 needs to have the correct object name, line 54 will need to have the correct API directory
@@ -80,3 +79,5 @@ function getDrList(){
 //     drDetailsContent.innerHTML = html;
 //     drDetailsContent.parentElement.classList.add('showDrInfo');
 // }
+
+//}
